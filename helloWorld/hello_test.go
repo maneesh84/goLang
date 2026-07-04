ef3144a1479d15275a2cf32b42f1e7908bbb7818
maneesh84 <maneeshkumar849896@gmail.com>
2026-07-04 18:31:33 +0530

    feat: enhance hello function to support multiple languages and update tests

--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -5,10 +5,59 @@ import (
 	// "hello"
 )
 
-func TestHello(t *testing.T) {
-	got := hello("maneesh")
-	want := "hello maneesh"
+// func TestHello(t *testing.T) {
+// 	got := hello("maneesh")
+// 	want := "hello maneesh"
 
+// 	if got != want {
+// 		t.Errorf("got %q want %q", got, want)
+// 	}
+// }
+
+// func TestMore(t *testing.T) {
+// 	t.Run("greeting pepole ", func(t *testing.T) {
+// 		got := hello("manee")
+// 		want := "hello manee"
+// 		if got != want {
+// 			t.Errorf("got %q want %q", got, want)
+// 		}
+// 	})
+// 	t.Run("greeting world ", func(t *testing.T) {
+// 		got := hello("")
+// 		want := "hello world"
+// 		if got != want {
+// 			t.Errorf("got %q want %q", got, want)
+// 		}
+// 	})
+// }
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := hello("Chris", "")
+		want := "Hello Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := hello("", "")
+		want := "Hello World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("greeting in Hindi", func(t *testing.T) {
+		got := hello("manee", "Hindi")
+		want := "Namaste manee"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("greeting in Jaapanees", func(t *testing.T) {
+		got := hello("manee", "Japanees")
+		want := "Konnichiwa manee"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
--- helloWorld/main.go
+++ helloWorld/main.go
@@ -1,13 +1,34 @@
 package main
 
-import (
-	"fmt"
-)
+const English = "English"
+const EnglishGreeting = "Hello "
 
-func hello(name string) string {
-	return "hello " + name
+const Hindi = "Hindi"
+const HindiGreeting = "Namaste "
+
+const Japanees = "Japanees"
+const JapaneesGreeting = "Konnichiwa "
+
+func hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return  greetPrefix(language ) +name
+}
+
+func greetPrefix(language string ) (greet string){
+	switch language {
+	case Hindi:
+		greet = HindiGreeting
+	case Japanees:
+		greet = JapaneesGreeting
+	default:
+		greet = EnglishGreeting
+	}
+	return
 }
 
 func main() {
-	fmt.Println(hello("maneesh"))
+	// fmt.Println(hello())
 }
