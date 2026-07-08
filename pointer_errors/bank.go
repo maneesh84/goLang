8c4368102d60fe3e8421cc7bcdf24de8725ebbe9
maneesh84 <maneeshkumar849896@gmail.com>
2026-07-08 16:11:09 +0530

    fix: update Bank methods to use Bitcoin type for balance and deposit

--- pointer_errors/bank.go
+++ pointer_errors/bank.go
@@ -2,15 +2,23 @@ package banksError
 
 import "fmt"
 
+// import "fmt"
+
 
 type Bank struct{
-	balance int
+	balance Bitcoin
 }
 
-func (b *Bank) Deposit(amount int ){
+func (b *Bank) Deposit(amount Bitcoin ){
 	b.balance+=amount
-	fmt.Printf("address of balance in Deposit is %p \n", &b.balance)
+	// fmt.Printf("address of balance in Deposit is %p \n", &b.balance)
 }
-func (b *Bank) Balance() int {
+func (b *Bank) Balance() Bitcoin {
 	return b.balance
+}
+
+type Bitcoin int 
+
+func (b Bitcoin) String() string{
+	return fmt.Sprintf("%d BTC",b)
 }
--- pointer_errors/bank_test.go
+++ pointer_errors/bank_test.go
@@ -8,13 +8,13 @@ import (
 func TestBank(t *testing.T){
 	bank:= Bank{}
 
-	bank.Deposit(10)
+	bank.Deposit(Bitcoin(10))
 
 	got:= bank.Balance()
 	fmt.Printf("address of balance in test is %p \n", &bank.balance)
-	want:= 10
+	want:=Bitcoin(10)
 
 	if(got!=want){
-		t.Errorf("got %d but want %d",got,want)
+		t.Errorf("got %s but want %s",got,want)
 	}
 }
