a2d8ece183b545c0d9521ef5ae92b00e60955dc8
maneesh84 <maneeshkumar849896@gmail.com>
2026-07-08 15:36:09 +0530

    fix: change Bank methods to use pointer receiver for Deposit and Balance

--- pointer_errors/bank.go
+++ pointer_errors/bank.go
@@ -7,10 +7,10 @@ type Bank struct{
 	balance int
 }
 
-func (b Bank) Deposit(amoutn int ){
-	b.balance+=amoutn
+func (b *Bank) Deposit(amount int ){
+	b.balance+=amount
 	fmt.Printf("address of balance in Deposit is %p \n", &b.balance)
 }
-func (b Bank) Balance() int {
+func (b *Bank) Balance() int {
 	return b.balance
 }
