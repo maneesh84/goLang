bb2c2633113d8d5aa451dba3b666e8c631cb40e9
maneesh84 <maneeshkumar849896@gmail.com>
2026-07-08 18:24:06 +0530

    fix: clean up unused imports and comments in Bank and test files

--- pointer_errors/bank.go
+++ pointer_errors/bank.go
@@ -2,9 +2,6 @@ package banksError
 
 import "fmt"
 
-// import "fmt"
-
-
 type Bank struct{
 	balance Bitcoin
 }
@@ -13,9 +10,19 @@ func (b *Bank) Deposit(amount Bitcoin ){
 	b.balance+=amount
 	// fmt.Printf("address of balance in Deposit is %p \n", &b.balance)
 }
+
+func (b *Bank) Withdraw(amount Bitcoin){
+	b.balance-=amount
+}
+
+
+
 func (b *Bank) Balance() Bitcoin {
 	return b.balance
 }
+
+
+
 
 type Bitcoin int 
 
--- pointer_errors/bank_test.go
+++ pointer_errors/bank_test.go
@@ -1,7 +1,7 @@
 package banksError
 
 import (
-	"fmt"
+
 	"testing"
 )
 
@@ -9,10 +9,10 @@ func TestBank(t *testing.T){
 	bank:= Bank{}
 
 	bank.Deposit(Bitcoin(10))
-
+	bank.Withdraw(Bitcoin(5))
 	got:= bank.Balance()
-	fmt.Printf("address of balance in test is %p \n", &bank.balance)
-	want:=Bitcoin(10)
+	// fmt.Printf("address of balance in test is %p \n", &bank.balance)
+	want:=Bitcoin(5)
 
 	if(got!=want){
 		t.Errorf("got %s but want %s",got,want)
