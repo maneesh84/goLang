8c2522c4cc5b650ec0029e6b8ae746e5240299cd
maneesh84 <maneeshkumar849896@gmail.com>
2026-07-09 15:26:04 +0530

    fix: update Dictionary Search method to return error for unknown words and enhance tests

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,10 +5,21 @@ import "testing"
 func TestDictionary(t *testing.T){
 	t.Run("search", func(t *testing.T) {
 		dict := Dictionary{"test":"this is first test"}
-		got := dict.Search("test")
+		got,_ := dict.Search("test")
 		want:="this is first test"
 
 		assertkey(t,got,want)
+	})
+	t.Run("unknown word", func(t *testing.T)  {
+		dict := Dictionary{"test":"this is first test"}
+		_,err := dict.Search("word")
+		want:= "word is not in dictionary"
+		if(err==nil){
+			t.Fatalf("expect error but got nothing")
+		}
+
+		assertkey(t,err.Error(),want)
+
 	})
 }
 
--- maps/types.go
+++ maps/types.go
@@ -1,7 +1,16 @@
 package maps
 
+import "errors"
+
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(key string) string {
-	return d[key]
+func (d Dictionary) Search(key string) (string,error) {
+	definition, ok := d[key]
+
+	if(!ok){
+		return "", errors.New("word is not in dictionary")
+	}
+
+	return definition,nil
 }
